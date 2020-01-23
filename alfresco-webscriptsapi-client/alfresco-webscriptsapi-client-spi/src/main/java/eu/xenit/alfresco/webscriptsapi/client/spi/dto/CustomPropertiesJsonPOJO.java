df43c365a1393f9294d5e01657667f51150495dc
Zlatin Todorinski <zlatin.todorinski@xenit.eu>
2020-01-23 17:20:43 +0100

    Added missing annotation on CustomPropertiesJsonPOJO

--- alfresco-webscriptsapi-client/alfresco-webscriptsapi-client-spi/src/main/java/eu/xenit/alfresco/webscriptsapi/client/spi/dto/CustomPropertiesJsonPOJO.java
+++ alfresco-webscriptsapi-client/alfresco-webscriptsapi-client-spi/src/main/java/eu/xenit/alfresco/webscriptsapi/client/spi/dto/CustomPropertiesJsonPOJO.java
@@ -2,11 +2,17 @@ package eu.xenit.alfresco.webscriptsapi.client.spi.dto;
 
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CustomPropertiesJsonPOJO {
     /**
