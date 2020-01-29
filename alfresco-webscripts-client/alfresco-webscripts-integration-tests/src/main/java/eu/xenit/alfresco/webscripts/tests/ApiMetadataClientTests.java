05429268d1bfef274c78666491330a9b99923487
Zlatin Todorinski <zlatin.todorinski@xenit.eu>
2020-01-29 16:09:37 +0100

    Replaced hardcoded property qnames with constants from Ditto

--- alfresco-webscripts-client/alfresco-webscripts-integration-tests/src/main/java/eu/xenit/alfresco/webscripts/tests/ApiMetadataClientTests.java
+++ alfresco-webscripts-client/alfresco-webscripts-integration-tests/src/main/java/eu/xenit/alfresco/webscripts/tests/ApiMetadataClientTests.java
@@ -6,6 +6,7 @@ import eu.xenit.alfresco.webscripts.client.spi.ApiMetadataClient;
 import eu.xenit.alfresco.webscripts.client.spi.Metadata;
 import eu.xenit.alfresco.webscripts.client.spi.NodeLocatorClient;
 import eu.xenit.alfresco.webscripts.client.spi.NodeLocatorClient.Locator;
+import eu.xenit.testing.ditto.api.data.ContentModel;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -21,25 +22,27 @@ public interface ApiMetadataClientTests {
     @Test
     default void testGetMetadata() {
         Map<String, String> properties = new HashMap<>();
-        properties.put("{http://www.alfresco.org/model/content/1.0}creator" , "System");
-        properties.put("{http://www.alfresco.org/model/content/1.0}name" , "Company Home");
-        properties.put("{http://www.alfresco.org/model/system/1.0}store-protocol" , "workspace");
-        properties.put("{http://www.alfresco.org/model/system/1.0}store-identifier" , "SpacesStore");
-        properties.put("{http://www.alfresco.org/model/system/1.0}node-dbid" , "13");
-        properties.put("{http://www.alfresco.org/model/system/1.0}locale" , "en_US");
-        properties.put("{http://www.alfresco.org/model/content/1.0}modifier" , "System");
+        properties.put(ContentModel.Content.CREATOR.toString(), "System");
+        properties.put(ContentModel.Content.NAME.toString(), "Company Home");
+        properties.put(ContentModel.System.STORE_PROTOCOL.toString(), "workspace");
+        properties.put(ContentModel.System.STORE_IDENTIFIER.toString(), "SpacesStore");
+        properties.put(ContentModel.System.NODE_DBID.toString(), "13");
+        properties.put(ContentModel.System.LOCALE.toString(), "en_US");
+        properties.put(ContentModel.Content.MODIFIER.toString(), "System");
 
         String companyHomeNodeRef = nodeLocatorClient().get(Locator.COMPANY_HOME);
         Metadata metadata = apiMetadataClient().get(companyHomeNodeRef);
         final String date = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault())
                                             .format(LocalDateTime.now());
         assertThat(metadata).isNotNull();
-        assertThat(metadata.getType()).isEqualTo("{http://www.alfresco.org/model/content/1.0}folder");
+        assertThat(metadata.getType()).isEqualTo(ContentModel.Content.FOLDER.toString());
         assertThat(metadata.getNodeRef()).startsWith("workspace://SpacesStore/");
-        assertThat(metadata.getAspects()).contains("{http://www.alfresco.org/model/content/1.0}auditable");
+        assertThat(metadata.getAspects()).contains(ContentModel.Content.AUDITABLE.toString());
         assertThat(metadata.getProperties()).isNotEmpty();
         assertThat(metadata.getProperties()).containsAllEntriesOf(properties);
-        assertThat(metadata.getProperties().getOrDefault("{http://www.alfresco.org/model/content/1.0}created", null)).startsWith(date);
-        assertThat(metadata.getProperties().getOrDefault("{http://www.alfresco.org/model/content/1.0}modified", null)).startsWith(date);
+        assertThat(metadata.getProperties()
+                .getOrDefault(ContentModel.Content.CREATED.toString(), null)).startsWith(date);
+        assertThat(metadata.getProperties()
+                .getOrDefault(ContentModel.Content.MODIFIED.toString(), null)).startsWith(date);
     }
 }
