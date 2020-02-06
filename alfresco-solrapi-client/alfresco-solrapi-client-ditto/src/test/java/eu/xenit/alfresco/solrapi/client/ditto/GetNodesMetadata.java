d26d6cb3ed1cb72b50661cd164f5a8b088718b6c
Daan Kerkhofs <daan.kerkhofs@xenit.eu>
2020-02-06 10:32:52 +0100

    Include test to reproduce UnsupportedOperationException in FakeSolrApiClient#getParentPaths(node)

--- alfresco-solrapi-client/alfresco-solrapi-client-ditto/src/test/java/eu/xenit/alfresco/solrapi/client/ditto/GetNodesMetadata.java
+++ alfresco-solrapi-client/alfresco-solrapi-client-ditto/src/test/java/eu/xenit/alfresco/solrapi/client/ditto/GetNodesMetadata.java
@@ -5,11 +5,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import eu.xenit.alfresco.solrapi.client.spi.SolrApiClient;
+import eu.xenit.alfresco.solrapi.client.spi.dto.NodePathInfo;
 import eu.xenit.alfresco.solrapi.client.spi.dto.SolrNodeMetaData;
 import eu.xenit.alfresco.solrapi.client.spi.query.NodeMetaDataQueryParameters;
+import eu.xenit.testing.ditto.api.AlfrescoDataSet;
 import eu.xenit.testing.ditto.api.NodeView;
 import eu.xenit.testing.ditto.api.data.ContentModel.Content;
 import eu.xenit.testing.ditto.api.data.ContentModel.System;
+import eu.xenit.testing.ditto.api.model.Node;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
@@ -24,10 +27,16 @@ public class GetNodesMetadata {
                         .setTxnId(6L)
                         .withProperty(Content.NAME, "Company Home")
                         .withProperty(Content.OWNER, "System")
+                        .withProperty(System.NODE_UUID, "company_home")
                         .withAspects(Content.AUDITABLE),
                 new TestNode(6L, Content.CONTENT).setTxnId(6L),
                 new TestNode(7L, Content.CONTENT).setTxnId(6L),
-                new TestNode(20L, Content.CONTENT).setTxnId(6L))
+                new TestNode(20L, Content.CONTENT).setTxnId(6L),
+                new TestNode(79L, Content.FOLDER)
+                        .setTxnId(15L)
+                        .withProperty(Content.NAME, "Folder")
+                        .withProperty(Content.OWNER, "Neo")
+                        .withAspects(Content.AUDITABLE))
         );
 
         return nodeView;
@@ -87,6 +96,49 @@ public class GetNodesMetadata {
                     // Alfresco doesn't actually use these filters:
 //                    assertThat(node.getAclId()).isEqualTo(-1L);
 //                    assertThat(node.getChildAssocs()).isEmpty();
+                });
+    }
+
+    @Test
+    void getNodesMetadata_folderWithSubFolder() {
+        AlfrescoDataSet dataSet = AlfrescoDataSet.bootstrapAlfresco()
+                .skipToTransaction(15L)
+                .addTransaction(t -> {
+                    t.skipToNodeId(79L);
+                    Node folder = t.addNode(n -> {
+                        n.name("Folder");
+                        n.type(Content.FOLDER);
+                        n.property(Content.OWNER, "Neo");
+                    });
+                    t.skipToNodeId(89L);
+                    Node subFolder = t.addNode(folder, n -> {
+                        n.name("Sub Folder");
+                        n.type(Content.FOLDER);
+                        n.property(Content.OWNER, "Morpheus");
+                    });
+                    t.skipToNodeId(90L);
+                    t.addDocument(subFolder, n -> {
+                        n.name("document.txt");
+                        n.property(Content.OWNER, "Morpheus");
+                    });
+                })
+                .build();
+
+        SolrApiClient client = new FakeSolrApiClient(dataSet);
+        List<SolrNodeMetaData> nodesMetaData = client
+                .getNodesMetaData(new NodeMetaDataQueryParameters().withNodeIds(90L));
+
+        assertThat(nodesMetaData)
+                .hasOnlyOneElementSatisfying(node -> {
+                    assertThat(node.getId()).isEqualTo(90L);
+                    assertThat(node.getType()).isEqualTo("cm:content");
+                    assertThat(node.getTxnId()).isEqualTo(15L);
+                    assertThat(node.getProperties().get(Content.NAME.toString())).isEqualTo("document.txt");
+                    assertThat(node.getOwner()).isEqualTo("Morpheus");
+                    assertThat(node.getAspects()).contains(Content.AUDITABLE.toPrefixString());
+                    assertThat(node.getPaths()).extracting(NodePathInfo::getPath)
+                            .containsExactly("/{http://www.alfresco.org/model/content/1.0}Folder"
+                                    + "/{http://www.alfresco.org/model/content/1.0}Sub Folder");
                 });
     }
 }
