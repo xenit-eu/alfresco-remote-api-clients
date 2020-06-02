9c279b03039adc3455b164ed092f082daf4f10ea
Dries De Proost <dries.deproost@xenit.eu>
2020-06-02 13:20:09 +0200

    do not generate setters for SolrNodeMetaData

--- alfresco-solrapi-client/alfresco-solrapi-client-api/src/main/java/eu/xenit/alfresco/client/solrapi/api/model/SolrNodeMetaData.java
+++ alfresco-solrapi-client/alfresco-solrapi-client-api/src/main/java/eu/xenit/alfresco/client/solrapi/api/model/SolrNodeMetaData.java
@@ -2,14 +2,17 @@ package eu.xenit.alfresco.client.solrapi.api.model;
 
 import java.util.List;
 import java.util.Map;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Setter;
 import lombok.Value;
 import lombok.experimental.Accessors;
 
 @Data
 @AllArgsConstructor
 @Accessors(chain = true)
+@Setter(AccessLevel.NONE)
 public class SolrNodeMetaData
 {
     private long id;
