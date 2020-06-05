4d6a3b87f2961fd8627c8d0ee9989ead41934b65
Dries De Proost <dries.deproost@xenit.eu>
2020-06-05 16:13:16 +0200

    more null checks

--- alfresco-solrapi-client/alfresco-solrapi-client-spring/src/main/java/eu/xenit/alfresco/solrapi/client/spring/SpringModelMapper.java
+++ alfresco-solrapi-client/alfresco-solrapi-client-spring/src/main/java/eu/xenit/alfresco/solrapi/client/spring/SpringModelMapper.java
@@ -33,7 +33,8 @@ public class SpringModelMapper {
     public AclChangeSetList toApiModel(AclChangeSetListModel model) {
         Objects.requireNonNull(model, "Argument 'model' is required");
         return new AclChangeSetList(
-                model.getAclChangeSets().stream().map(this::toApiModel).collect(Collectors.toList()),
+                model.getAclChangeSets() == null ? null
+                        : model.getAclChangeSets().stream().map(this::toApiModel).collect(Collectors.toList()),
                 model.getMaxChangeSetCommitTime(),
                 model.getMaxChangeSetId());
     }
@@ -45,14 +46,15 @@ public class SpringModelMapper {
 
     public List<Acl> toApiModel(AclListModel model) {
         Objects.requireNonNull(model, "Argument 'model' is required");
-        return model.getAcls().stream()
+        return model.getAcls() == null ? null : model.getAcls().stream()
                 .map(acl -> new Acl(acl.getAclChangeSetId(), acl.getId(), acl.getInheritedId()))
                 .collect(Collectors.toList());
     }
 
     public List<AclReaders> toApiModel(AclReadersListModel model) {
         Objects.requireNonNull(model, "Argument 'model' is required");
-        return model.getAclsReaders().stream().map(this::toApiModel).collect(Collectors.toList());
+        return model.getAclsReaders() == null ? null
+                : model.getAclsReaders().stream().map(this::toApiModel).collect(Collectors.toList());
     }
 
     private AclReaders toApiModel(AclReadersModel model) {
@@ -63,7 +65,8 @@ public class SpringModelMapper {
 
     public List<SolrNode> toApiModel(SolrNodeListModel model) {
         Objects.requireNonNull(model, "Argument 'model' is required");
-        return model.getNodes().stream().map(this::toApiModel).collect(Collectors.toList());
+        return model.getNodes() == null ? null
+                : model.getNodes().stream().map(this::toApiModel).collect(Collectors.toList());
     }
 
     private SolrNode toApiModel(SolrNodeModel model) {
@@ -75,7 +78,8 @@ public class SpringModelMapper {
     public SolrTransactions toApiModel(SolrTransactionsModel model) {
         Objects.requireNonNull(model, "Argument 'model' is required");
         return new SolrTransactions(
-                model.getTransactions().stream().map(this::toApiModel).collect(Collectors.toList()),
+                model.getTransactions() == null ? null
+                        : model.getTransactions().stream().map(this::toApiModel).collect(Collectors.toList()),
                 model.getMaxTxnCommitTime(),
                 model.getMaxTxnId());
     }
@@ -87,7 +91,8 @@ public class SpringModelMapper {
 
     public List<SolrNodeMetadata> toApiModel(SolrNodeMetadataListModel model) {
         Objects.requireNonNull(model, "Argument 'model' is required");
-        return model.getNodes().stream().map(this::toApiModel).collect(Collectors.toList());
+        return model.getNodes() == null ? null
+                : model.getNodes().stream().map(this::toApiModel).collect(Collectors.toList());
     }
 
     SolrNodeMetadata toApiModel(SolrNodeMetadataModel model) {
